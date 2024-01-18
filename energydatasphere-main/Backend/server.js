b7ac4abecf7985269ff387d0e349da25bc944e14
Eyuel Kahsay <125311214+Eyu108@users.noreply.github.com>
2024-01-18 12:23:43 -0700

    Update server.js

--- energydatasphere-main/Backend/server.js
+++ energydatasphere-main/Backend/server.js
@@ -14,7 +14,6 @@ wss.on('connection', (ws) => {
   // ... WebSocket code ...
 });
 
-// Simple root route (optional)
 app.get('/', (req, res) => {
   res.send('Welcome to Energy Data Sphere');
 });
@@ -24,17 +23,15 @@ app.get('/api/data', async (req, res) => {
 });
 
 // Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, '../frontend')));
+app.use(express.static(path.join(__dirname, '../frontend/build')));
 
 // Anything that doesn't match the above, send back the index.html file
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../frontend', 'App.js'));
+  res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'));
 });
 
 // Periodically fetch new data
 setInterval(fetchDataFromCommoditiesAPI, 3600000); // Fetch new data every hour
-
-// ... Existing server setup code ...
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
